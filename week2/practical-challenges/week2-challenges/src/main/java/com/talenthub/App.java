e63da7a8e5506f7a99a2f2feb9661da6f4d996e4
Musarnt <samuelkrd10@gmail.com>
2026-04-01 06:49:57 -0500

    feat: initialize week2 Maven project structure

---
+++ week2/practical-challenges/week2-challenges/src/main/java/com/talenthub/App.java
@@ -0,0 +1,13 @@
+package com.talenthub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
